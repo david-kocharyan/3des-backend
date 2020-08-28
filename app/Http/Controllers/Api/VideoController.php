47eb38df7de97d05e3e5ca4f9dff2574d0b965b4
David <david@aimtech.am>
2020-08-28 12:45:23 +0400

    video api concat fix 1

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -14,7 +14,7 @@ class VideoController extends Controller
      */
     public function index()
     {
-        $data = Video::selectRaw("CONCAT('uploads','/',link) AS link'")->orderBy("id", "ASC")->first();
+        $data = Video::selectRaw("CONCAT('uploads','/',link) AS link")->orderBy("id", "ASC")->first();
         if (null == $data) {
             return ResponseHelper::fail("Video Not Found", 422);
         }
