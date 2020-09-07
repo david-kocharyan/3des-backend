0280497836f9a5c40b0cc20cb033c3fa4319e8d4
David <david@aimtech.am>
2020-09-07 20:21:56 +0400

    some fix

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'Api', "prefix" => "v1"], function () {
 
-    Route::group(['prefix' => 'auth'], function () {
+    Route::group(['prefix' => 'user'], function () {
         Route::post('signin', 'AuthController@login');
         Route::post('signup', 'AuthController@signup');
 
